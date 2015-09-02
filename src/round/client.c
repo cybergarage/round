96aee1901e219ab3eecd1157a149376a74cde06f
Satoshi Konno <skonno@yahoo-corp.jp>
2015-09-02 18:56:35 +0900

    * Added skelton projects.

---
+++ src/round/client.c
@@ -0,0 +1,17 @@
+/******************************************************************
+ *
+ * Round SDK for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <round/client.h>
+
+int round_client()
+{
+  return 0;
+}
+
