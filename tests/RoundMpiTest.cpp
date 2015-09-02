88f243bc758679d181ad30cd21e5bd6c77489398
Satoshi Konno <skonno@cybergarage.org>
2015-09-02 18:56:35 +0900

    * Added skelton projects.

---
+++ tests/RoundMpiTest.cpp
@@ -0,0 +1,15 @@
+/******************************************************************
+ *
+ * Round SDK for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#define BOOST_TEST_MODULE RoundSdkTest
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
