f31c61fe7188c2eb103a01ae5beec0a052997506
Satoshi Konno <skonno@cybergarage.org>
2015-09-05 18:10:11 +0900

    * Added finder_internal.h.

---
+++ include/round/finder.h
@@ -0,0 +1,47 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _ROUNDC_FINDER__H_
+#define _ROUNDC_FINDER__H_
+
+#include <round/typedef.h>
+#include <round/const.h>
+
+#include <mupnp/controlpoint.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+/****************************************
+ * Data Type
+ ****************************************/
+  
+#if !defined(_ROUNDC_FINDER_INTERNAL_H_)
+typedef void RoundFinder;
+#endif
+  
+/****************************************
+ * Function
+ ****************************************/
+  
+RoundFinder *round_finder_new(void);
+bool round_finder_delete(RoundFinder *finder);
+
+bool round_finder_start(RoundFinder *finder);
+bool round_finder_stop(RoundFinder *finder);
+
+bool round_finder_search(RoundFinder *finder);
+
+#ifdef  __cplusplus
+} /* extern C */
+#endif
+
+#endif /* _ROUNDC_FINDER__H_ */
--- src/round/node_internal.h
+++ src/round/node_internal.h
@@ -29,7 +29,7 @@ typedef struct _RoundNode {
 } RoundNode, RoundNodeList;
 
 /****************************************
- * Header
+ * Public Header
  ****************************************/
   
 #include <round/node.h>
