0a08841b1cdd28cd5ac0d9599c4488b83463f623
Satoshi Konno <skonno@cybergarage.org>
2015-09-10 01:27:09 +0900

    * Ported semaphore functions form Round C++.

---
+++ src/round/util/semaphore.h
@@ -0,0 +1,62 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _ROUNDC_UTIL_SEMAPHORE_H_
+#define _ROUNDC_UTIL_SEMAPHORE_H_
+
+#include <round/typedef.h>
+#include <time.h>
+
+#if defined(__APPLE__)
+#include <mach/mach.h>
+#include <mach/semaphore.h>
+#else
+#include <pthread.h>
+#include <semaphore.h>
+#endif
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+/****************************************
+ * Data Types
+ ****************************************/
+
+#if defined(__APPLE__)
+  typedef semaphore_t SemaphoreId;
+#else
+  typedef sem_t SemaphoreId;
+#endif
+  
+typedef struct {
+  SemaphoreId semId;
+  bool isInitialized;
+  size_t maxCnt;
+} RoundSemaphore;
+
+/****************************************
+ * Functions
+ ****************************************/
+
+RoundSemaphore *round_semaphore_new(size_t max);
+bool round_semaphore_delete(RoundSemaphore *sem);
+
+bool round_semaphore_post(RoundSemaphore *sem);
+bool round_semaphore_reset(RoundSemaphore *sem);
+bool round_semaphore_wait(RoundSemaphore *sem);
+bool round_semaphore_timedwait(RoundSemaphore *sem, time_t timeoutSec);
+bool round_semaphore_cancel(RoundSemaphore *sem);
+
+#ifdef  __cplusplus
+} /* extern "C" */
+#endif
+
+#endif
---
+++ tests/SemaphoreTest.cpp
@@ -0,0 +1,86 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+
+#include <round/util/semaphore.h>
+#include <round/util/thread.h>
+#include <round/util/timer.h>
+
+BOOST_AUTO_TEST_SUITE(semaphore)
+
+BOOST_AUTO_TEST_CASE(RoundSemaphoreBasicTest) {
+  RoundSemaphore *sem = round_semaphore_new(1);
+  BOOST_CHECK(sem);
+  
+  BOOST_CHECK_EQUAL(round_semaphore_post(sem), true);
+  BOOST_CHECK_EQUAL(round_semaphore_wait(sem), true);
+
+  BOOST_CHECK_EQUAL(round_semaphore_post(sem), true);
+  BOOST_CHECK_EQUAL(round_semaphore_post(sem), true);
+  BOOST_CHECK_EQUAL(round_semaphore_wait(sem), true);
+  BOOST_CHECK_EQUAL(round_semaphore_wait(sem), true);
+
+  BOOST_CHECK_EQUAL(round_semaphore_post(sem), true);
+  BOOST_CHECK_EQUAL(round_semaphore_wait(sem), true);
+  BOOST_CHECK_EQUAL(round_semaphore_post(sem), true);
+  BOOST_CHECK_EQUAL(round_semaphore_wait(sem), true);
+
+  BOOST_CHECK(round_semaphore_delete(sem));
+}
+
+BOOST_AUTO_TEST_CASE(RoundSemaphoreMaxCountTest)
+{
+  for (size_t semMaxCount = 1; semMaxCount<10; semMaxCount++) {
+    RoundSemaphore *sem = round_semaphore_new(semMaxCount);
+    BOOST_CHECK(sem);
+
+    for (size_t n=0; n<semMaxCount; n++) {
+      BOOST_CHECK_EQUAL(round_semaphore_wait(sem), true);
+    }
+    
+    for (size_t n=0; n<semMaxCount; n++) {
+      BOOST_CHECK_EQUAL(round_semaphore_post(sem), true);
+    }
+    
+    BOOST_CHECK(round_semaphore_delete(sem));
+  }
+}
+
+static const int SEMAPHORE_THREAD_TEST_LOOP_NUM = 10;
+
+void RoundSemaphorePostThread(RoundThread *thread)
+{
+  RoundSemaphore *sem = (RoundSemaphore *)round_thread_getuserdata(thread);
+  for (int n = 0; n < SEMAPHORE_THREAD_TEST_LOOP_NUM; n++) {
+    round_sleep(100);
+    BOOST_CHECK_EQUAL(round_semaphore_post(sem), true);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(RoundSemaphoreThreadTest)
+{
+  RoundSemaphore *sem = round_semaphore_new(0);
+  
+  RoundThread *thread = round_thread_new();
+  round_thread_setaction(thread, RoundSemaphorePostThread);
+  round_thread_setuserdata(thread, sem);
+  BOOST_CHECK(round_thread_start(thread));
+  
+  for (int n = 0; n < SEMAPHORE_THREAD_TEST_LOOP_NUM; n++) {
+    BOOST_CHECK_EQUAL(round_semaphore_wait(sem), true);
+  }
+
+  BOOST_CHECK(round_thread_stop(thread));
+  BOOST_CHECK(round_thread_delete(thread));
+  BOOST_CHECK(round_semaphore_delete(sem));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
