f709e063d8ac7b3824b203f02caf7828c5d42b26
Satoshi Konno <skonno@cybergarage.org>
2015-09-09 12:08:04 +0900

    * Updated json functions.

--- src/round/util/json_object.c
+++ src/round/util/json_object.c
@@ -8,7 +8,7 @@
  *
  ******************************************************************/
 
-#include <round/util/json.h>
+#include <round/util/json_internal.h>
 
 /****************************************
  * round_json_object_new
@@ -22,6 +22,10 @@ RoundJSONObject *round_json_object_new(void)
   
   if (!obj)
     return NULL;
+
+#if defined(ROUND_USE_JSON_PARSER_JANSSON)
+  obj->jsonObj = NULL;
+#endif
   
   return obj;
 }
@@ -35,7 +39,55 @@ bool round_json_object_delete(RoundJSONObject *obj)
   if (!obj)
     return false;
   
+#if defined(ROUND_USE_JSON_PARSER_JANSSON)
+  if (obj->jsonObj) {
+    json_decref(obj->jsonObj);
+    obj->jsonObj = NULL;
+  }
+#endif
+  
   free(obj);
   
   return true;
+}
+
+/****************************************
+ * round_json_object_delete
+ ****************************************/
+
+int round_json_object_gettype(RoundJSONObject *obj)
+{
+  if (!obj)
+    return ROUND_JSON_UNKOWN;
+  
+  switch (json_typeof(obj->jsonObj)) {
+    case JSON_OBJECT:
+      return ROUND_JSON_MAP;
+    case JSON_ARRAY:
+      return ROUND_JSON_ARRAY;
+    case JSON_NULL:
+      return ROUND_JSON_NULL;
+    case JSON_STRING:
+      return ROUND_JSON_STRING;
+    case JSON_INTEGER:
+      return ROUND_JSON_INTEGER;
+    case JSON_REAL:
+      return ROUND_JSON_REAL;
+    case JSON_TRUE:
+    case JSON_FALSE:
+      return ROUND_JSON_BOOL;
+  }
+
+  return ROUND_JSON_UNKOWN;
+}
+
+/****************************************
+ * round_json_object_delete
+ ****************************************/
+
+bool round_json_object_istype(RoundJSONObject *obj, int type)
+{
+  if (!obj)
+    return false;
+  return (round_json_object_gettype(obj) == type) ? true : false;
 }
