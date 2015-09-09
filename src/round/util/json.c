d5d60761d96c84690293d75a1a296829b39d1c4d
Satoshi Konno <skonno@cybergarage.org>
2015-09-09 12:56:23 +0900

    * Updated JSON functions.

--- src/round/util/json.c
+++ src/round/util/json.c
@@ -9,6 +9,7 @@
  ******************************************************************/
 
 #include <round/util/json_internal.h>
+#include <round/util/strings.h>
 
 /****************************************
 * round_json_new
@@ -92,6 +93,9 @@ bool round_json_parse(RoundJSON *json, const char *jsonStr, RoundError *err)
   if (!json || !jsonStr)
     return false;
   
+  if (!round_json_clear(json))
+    return false;
+  
 #if defined(ROUND_USE_JSON_PARSER_JANSSON)
   json_t *jsonRes = json_loads(jsonStr, 0, &jsonErr);
   if (!jsonRes) {
@@ -106,10 +110,113 @@ bool round_json_parse(RoundJSON *json, const char *jsonStr, RoundError *err)
     json_decref(jsonRes);
     return false;
   }
-  round_json_object_setjanssonobject(jsonObj, jsonRes);
-  
+
   round_json_setrootobject(json, jsonObj);
+  round_json_object_setjanssonobject(jsonObj, jsonRes);
 #endif
   
   return false;
+}
+
+
+/****************************************
+ * round_json_getobjectforpath
+ ****************************************/
+
+#if defined(ROUND_USE_JSON_PARSER_JANSSON)
+
+json_t *round_jansson_map_getobject(json_t *jsonMap, const char *name) {
+  const char *jsonKey;
+  json_t *jsonObj;
+
+  if (!jsonMap || !name)
+    return NULL;
+  
+#if defined(ROUND_USE_JANSSON_JSON_OBJECT_FOREACH)
+  // New in version 2.3
+  json_object_foreach(jsonMap, jsonKey, jsonObj) {
+#else
+  for (void *it = json_object_iter(jsonMap); it; it = json_object_iter_next(jsonMap, it)) {
+    jsonKey = json_object_iter_key(it);
+    jsonObj = json_object_iter_value(it);
+#endif
+    if (round_streq(jsonKey, name))
+      return jsonObj;
+  }
+
+  return NULL;
+}
+  
+json_t *round_jansson_array_getobject(json_t *jsonArray, size_t n)
+{
+  size_t jsonIdx;
+  json_t *jsonObj;
+  size_t jsonArraySize;
+  
+  if (!jsonArray)
+    return NULL;
+  
+  jsonArraySize = json_array_size(jsonArray);
+  if ((jsonArraySize - 1) < n)
+    return NULL;
+
+  if (n < jsonArraySize)
+    return json_array_get(jsonArray, n);
+  
+#if defined(ROUND_USE_JANSSON_JSON_ARRAY_FOREACH)
+  // New in version 2.5.
+  json_array_foreach(jsonArray, jsonIdx, jsonObj) {
+#else
+  for (jsonIdx =0; jsonIdx < jsonArraySize; jsonIdx++) {
+    jsonObj = json_array_get(jsonArray, jsonIdx);
+#endif
+  }
+
+  return NULL;
+}
+#endif
+
+RoundJSONObject *round_json_getobjectforpath(RoundJSON *json, const char *pathStr)
+{
+  char *path, *token, *ptr;
+#if defined(ROUND_USE_JSON_PARSER_JANSSON)
+  json_t *rootJson, *tokenJson;
+#endif
+  
+  if (!json || !pathStr)
+    return NULL;
+
+  if (!json->rootObj)
+    return NULL;
+  
+#if defined(ROUND_USE_JSON_PARSER_JANSSON)
+  rootJson = json->rootObj->jsonObj;
+  if (!rootJson)
+    return NULL;
+#endif
+
+  path = round_strdup(pathStr);
+  if (!path)
+    return NULL;
+  
+  token = round_strtok(path, ROUND_JSON_PATH_DELIM, &ptr);
+  
+  if (!token)
+    return NULL;
+  
+#if defined(ROUND_USE_JSON_PARSER_JANSSON)
+
+  if (!json_is_object(rootJson))
+    return NULL;
+  
+  tokenJson = NULL;
+  
+  while (token) {
+    token = round_strtok(NULL, ROUND_JSON_PATH_DELIM, &ptr);
+  }
+
+  round_json_object_setjanssonobject(json->pathObj, tokenJson);
+#endif
+
+  return json->pathObj;
 }
