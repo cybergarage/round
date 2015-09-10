77c9adf5487f09e43c24756359711c9881bb5de4
Satoshi Konno <skonno@cybergarage.org>
2015-09-10 14:06:13 +0900

    * Updated node functions.

--- src/round/node.c
+++ src/round/node.c
@@ -50,3 +50,39 @@ RoundNode *round_node_next(RoundNode *node)
 {
   return (RoundNode *)round_list_next((RoundList *)node);
 }
+
+/****************************************
+ * round_node_getrequestaddress
+ ****************************************/
+
+bool round_node_getrequestaddress(RoundNode *node, const char **address, RoundError *err)
+{
+  return false;
+}
+
+/****************************************
+ * round_node_getrequestport
+ ****************************************/
+
+bool round_node_getrequestport(RoundNode *node, int *port, RoundError *err)
+{
+  return false;
+}
+
+/****************************************
+ * round_node_getclustername
+ ****************************************/
+
+bool round_node_getclustername(RoundNode *node, const char **address, RoundError *err)
+{
+  return false;
+}
+
+/****************************************
+ * round_node_postmessage
+ ****************************************/
+
+bool round_node_postmessage(RoundJSONObject *reqObj, RoundJSONObject *resObj, RoundError *err)
+{
+  return false;
+}
