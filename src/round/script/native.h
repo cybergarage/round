cbc95282553e6f2a841de083148c3771f974acbf
Satoshi Konno <skonno@cybergarage.org>
2015-09-21 19:29:58 +0900

    * Added naitive script engine.

---
+++ src/round/script/native.h
@@ -0,0 +1,37 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _ROUNDC_NATIVE_ENGINE_H_
+#define _ROUNDC_NATIVE_ENGINE_H_
+
+#include <round/script.h>
+#include <round/method.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+  
+#define RoundNativeEngineLanguage "c"
+  
+typedef struct {
+  ROUND_SCRIPT_ENGINE_STRUCT_MEMBERS
+} RoundNativeEngine;
+  
+RoundNativeEngine *round_native_engine_new();
+bool round_native_engine_destory(RoundNativeEngine *engine);
+bool round_native_engine_delete(RoundNativeEngine *engine);
+  
+bool round_native_engine_run(RoundNativeEngine *engine, RoundMethod *method, const char *param, RoundString *result, RoundError *err);
+  
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
