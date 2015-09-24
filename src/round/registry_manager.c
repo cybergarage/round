36ce3584527beae47610710ef77a14882caea09c
Satoshi Konno <skonno@cybergarage.org>
2015-09-25 03:19:23 +0900

    * Added RegistryManager.

---
+++ src/round/registry_manager.c
@@ -0,0 +1,47 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <stdlib.h>
+#include <round/const.h>
+#include <round/error_internal.h>
+#include <round/registry.h>
+
+/****************************************
+ * round_registry_manager_new
+ ****************************************/
+
+RoundRegistryManager *round_registry_manager_new()
+{
+  RoundRegistryManager *mgr;
+  
+  mgr = (RoundRegistryManager *)malloc(sizeof(RoundRegistryManager));
+  if (!mgr)
+    return NULL;
+  
+  mgr->map  = round_registry_map_new();
+  
+  return mgr;  
+}
+
+/****************************************
+ * round_registry_manager_delete
+ ****************************************/
+
+bool round_registry_manager_delete(RoundRegistryManager *mgr)
+{
+  if (!mgr)
+    return false;
+  
+  round_registry_map_delete(mgr->map);
+  
+  free(mgr);
+  
+  return true;
+}
---
+++ src/round/registry_map.c
@@ -0,0 +1,29 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <stdlib.h>
+#include <round/registry.h>
+
+/****************************************
+ * round_registry_map_new
+ ****************************************/
+
+RoundMap *round_registry_map_new()
+{
+  RoundMap *map;
+  
+  map = round_map_new();
+  if (!map)
+    return NULL;
+  
+  round_map_setmapobjectdestructor(map, (ROUND_MAP_OBJECT_DESTRUCTOR)round_registry_delete);
+  
+  return map;
+}
