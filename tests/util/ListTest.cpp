c5bd212cec2cbc07e93cafbaba8065d4536b3d58
Satoshi Konno <skonno@cybergarage.org>
2015-10-20 00:59:21 +0900

    * Updated list functions.

--- tests/util/ListTest.cpp
+++ tests/util/ListTest.cpp
@@ -12,6 +12,23 @@
 
 #include <round/util/list.h>
 
-BOOST_AUTO_TEST_CASE(ListNew)
+#define ROUND_TEST_LIST_SIZE 10
+
+BOOST_AUTO_TEST_CASE(ListAdd)
 {
+  RoundList *nodes = round_list_header_new();
+  
+  BOOST_CHECK(nodes);
+  BOOST_CHECK_EQUAL(0, round_list_size(nodes));
+  
+  RoundList *node[ROUND_TEST_LIST_SIZE];
+  for (int n=0; n<ROUND_TEST_LIST_SIZE; n++) {
+    node[n] = round_list_node_new();
+    BOOST_CHECK(round_list_add(nodes, node[n]));
+    BOOST_CHECK_EQUAL((n+1), round_list_size(nodes));
+  }
+  
+  BOOST_CHECK_EQUAL(ROUND_TEST_LIST_SIZE, round_list_size(nodes));
+  
+  BOOST_CHECK(round_list_delete(nodes));
 }
