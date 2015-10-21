75ff4230a0f2c581c6e031f0df587af173a67a35
Satoshi Konno <skonno@cybergarage.org>
2015-10-21 18:32:44 +0900

    * Added ENDL.

--- include/round/const.h
+++ include/round/const.h
@@ -16,6 +16,7 @@ extern "C" {
 #endif
 
 #define ROUNDC_SYSTEM_TRIGGER_TIMER_PRECISION 0.1
+#define ROUNDC_ENDL "\n"
 #define ROUNDC_FINDER_UPNP_DEVICE_TYPE "urn:cybergarage-org:device:round:1"
 #define ROUNDC_FINDER_UPNP_SERVICE_TYPE "urn:cybergarage-org:service:round:1"
 #define ROUNDC_FINDER_UPNP_ACTION_GETPRODUCTINFO "GetProductInfo"
