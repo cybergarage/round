aa7f47422de1a1accd0c75e8359a2f134a40c8f3
Satoshi Konno <skonno@cybergarage.org>
2015-10-22 21:54:05 +0900

    * Adde tests for mutex functions.

---
+++ tests/util/MutexTest.cpp
@@ -0,0 +1,22 @@
+/******************************************************************
+*
+* Round for C++
+*
+* Copyright (C) Satoshi Konno 2015
+*
+* This is licensed under BSD-style license, see file COPYING.
+*
+******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+#include <roundc/round.h>
+
+BOOST_AUTO_TEST_CASE(MutexTest) {
+  RoundMutex *mutex;
+  
+  mutex = round_mutex_new();
+  BOOST_CHECK(mutex);
+  BOOST_CHECK(round_mutex_lock(mutex));
+  BOOST_CHECK(round_mutex_unlock(mutex));
+  BOOST_CHECK(round_mutex_delete(mutex));
+}
