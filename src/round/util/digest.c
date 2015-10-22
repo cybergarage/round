e968fdb670c84b90cb82b27c47e7303e9bf10547
Satoshi Konno <skonno@cybergarage.org>
2015-10-22 21:59:05 +0900

    * Added digest functions.

---
+++ src/round/util/digest.c
@@ -0,0 +1,102 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+#if defined(__APPLE__)
+#define COMMON_DIGEST_FOR_OPENSSL
+#include <CommonCrypto/CommonDigest.h>
+#define SHA1FUNC CC_SHA1
+#define SHA256FUNC CC_SHA256
+#else
+#include <openssl/sha.h>
+#define SHA1FUNC SHA1
+#define SHA256FUNC SHA256
+#endif
+
+#include <round/util/digest.h>
+#include <round/util/strings.h>
+
+////////////////////////////////////////
+// MD5
+////////////////////////////////////////
+
+#if defined(ROUND_SUPPORT_MD_5DIGEST)
+
+bool round_md5_digest(const char *string, char **buf)
+{
+  char *md5DigestString = (char *)malloc((MD5_DIGEST_LENGTH*2) + 1);
+  
+  unsigned char md5Digest[MD5_DIGEST_LENGTH];
+  MD5FUNC(string, (int)round_strlen(string), md5Digest);
+  
+  char hexString[3];
+  for (int n = 0; n < MD5_DIGEST_LENGTH; n++) {
+    snprintf(hexString, sizeof(hexString), "%02X", md5Digest[n]);
+    md5DigestString[(n*2)+0] = hexString[0];
+    md5DigestString[(n*2)+1] = hexString[1];
+  }
+  md5DigestString[(MD5_DIGEST_LENGTH*2)] = '\0';
+  
+  *buf = md5DigestString;
+
+  return true;
+}
+
+#endif
+
+////////////////////////////////////////
+// SHA1
+////////////////////////////////////////
+
+bool round_sha_digest(const char *string, char **buf)
+{
+  unsigned char shaDigest[SHA_DIGEST_LENGTH];
+  char *shaDigestString = (char *)malloc((SHA_DIGEST_LENGTH*2) + 1);
+
+  SHA1FUNC(string, (int)round_strlen(string), shaDigest);
+  
+  char hexString[3];
+  for (int n = 0; n < SHA_DIGEST_LENGTH; n++) {
+    snprintf(hexString, sizeof(hexString), "%02X", shaDigest[n]);
+    shaDigestString[(n*2)+0] = hexString[0];
+    shaDigestString[(n*2)+1] = hexString[1];
+  }
+  shaDigestString[(SHA_DIGEST_LENGTH*2)] = '\0';
+  
+  *buf = shaDigestString;
+
+  return true;
+}
+
+////////////////////////////////////////
+// SHA256
+////////////////////////////////////////
+
+bool round_sha256_digest(const char *string, char **buf)
+{
+  char *shaDigestString = (char *)malloc((SHA256_DIGEST_LENGTH*2) + 1);
+  
+  unsigned char shaDigest[SHA256_DIGEST_LENGTH];
+  SHA256FUNC(string, (int)round_strlen(string), shaDigest);
+  
+  char hexString[3];
+  for (int n = 0; n < SHA256_DIGEST_LENGTH; n++) {
+    snprintf(hexString, sizeof(hexString), "%02X", shaDigest[n]);
+    shaDigestString[(n*2)+0] = hexString[0];
+    shaDigestString[(n*2)+1] = hexString[1];
+  }
+  shaDigestString[(SHA256_DIGEST_LENGTH*2)] = '\0';
+  
+  *buf = shaDigestString;
+
+  return true;
+}
---
+++ tests/DigestTest.cpp
@@ -0,0 +1,49 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+
+#include <round/util/digest.h>
+#include <round/util/strings.h>
+
+BOOST_AUTO_TEST_SUITE(digest)
+
+#if defined(ROUND_SUPPORT_MD_5DIGEST)
+
+BOOST_AUTO_TEST_CASE(MD5Digest)
+{
+  char *digest = NULL;
+  BOOST_CHECK(round_md5_digest("a", &digest));
+  BOOST_CHECK(digest);
+  BOOST_CHECK_EQUAL(round_strlen(digest), ROUNDC_MD5_DIGEST_STRING_LENGTH);
+  free(digest);
+}
+
+#endif
+
+BOOST_AUTO_TEST_CASE(SHADigest)
+{
+  char *digest = NULL;
+  BOOST_CHECK(round_sha_digest("a", &digest));
+  BOOST_CHECK(digest);
+  BOOST_CHECK_EQUAL(round_strlen(digest), ROUNDC_SHA_DIGEST_STRING_LENGTH);
+  free(digest);
+}
+
+BOOST_AUTO_TEST_CASE(SHA256Digest)
+{
+  char *digest = NULL;
+  BOOST_CHECK(round_sha256_digest("a", &digest));
+  BOOST_CHECK(digest);
+  BOOST_CHECK_EQUAL(round_strlen(digest), ROUNDC_SHA256_DIGEST_STRING_LENGTH);
+  free(digest);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
