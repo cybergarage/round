eb67f308bae19964cdfaf493b7f22219a1663863
Satoshi Konno <skonno@cybergarage.org>
2015-11-19 23:16:26 +0900

    * Updated UPnP Server.

--- src/round/rpc_server.c
+++ src/round/rpc_server.c
@@ -8,7 +8,13 @@
  *
  ******************************************************************/
 
+#include <round/const.h>
 #include <round/rpc_server.h>
+#include <round/error_internal.h>
+#include <round/util/strings.h>
+#include <round/util/json.h>
+
+void round_rpc_server_jsonrpcrequestrecieved(mUpnpHttpRequest *httpReq);
 
 /****************************************
 * round_rpc_server_new
@@ -30,6 +36,8 @@ RoundRpcServer *round_rpc_server_new(void)
     return NULL;
   }
 
+  round_upnp_server_setrpcrequestlistener(server->upnpServer, round_rpc_server_jsonrpcrequestrecieved);
+  
   return server;
 }
 
@@ -103,3 +111,70 @@ bool round_rpc_server_isrunning(RoundRpcServer *server)
 
   return true;
 }
+
+/****************************************
+ * round_rpc_server_jsonrpcrequestrecieved
+ ****************************************/
+
+void round_rpc_server_posterrorresponse(mUpnpHttpRequest *httpReq, int rpcErrCode)
+{
+  mUpnpHttpResponse *httpRes = mupnp_http_response_new();
+  mupnp_http_response_setstatuscode(httpRes, round_json_rpc_errorcode2httpstatuscode(rpcErrCode));
+  
+  // TODO : Set JSON response content
+  //RoundError *err = round_error_new();
+  //round_error_setjsonrpcerrorcode(err, rpcErrCode);
+  
+  mupnp_http_request_postresponse(httpReq, httpRes);
+  mupnp_http_response_delete(httpRes);
+}
+
+/****************************************
+ * round_rpc_server_hasjsonrpcparameters
+ ****************************************/
+
+bool round_rpc_server_hasjsonrpcparameters(RoundJSON *json)
+{
+  RoundJSONObject *rootObj = round_json_getrootobject(json);
+  if (!rootObj)
+    return false;
+  
+  if (!round_json_object_isarray(json))
+    return false;
+  
+  return true;
+}
+
+/****************************************
+ * round_rpc_server_jsonrpcrequestrecieved
+ ****************************************/
+
+void round_rpc_server_jsonrpcrequestrecieved(mUpnpHttpRequest *httpReq)
+{
+  const char *jsonContent = mupnp_http_request_getcontent(httpReq);
+  if (jsonContent) {
+    round_rpc_server_posterrorresponse(httpReq, ROUNDC_RPC_ERROR_CODE_INVALID_REQUEST);
+    return;
+  }
+  
+  RoundJSON *json = round_json_new();
+  if (!json) {
+    round_rpc_server_posterrorresponse(httpReq, ROUNDC_RPC_ERROR_CODE_INTERNAL_ERROR);
+    return;
+  }
+  
+  if (round_json_parse(json, jsonContent, NULL)) {
+    round_json_delete(json);
+    round_rpc_server_posterrorresponse(httpReq, ROUNDC_RPC_ERROR_CODE_PARSER_ERROR);
+    return;
+  }
+  
+  if (!round_rpc_server_hasjsonrpcparameters(json)) {
+    round_json_delete(json);
+    round_rpc_server_posterrorresponse(httpReq, ROUNDC_RPC_ERROR_CODE_PARSER_ERROR);
+    return;
+  }
+  
+  round_json_delete(json);
+}
+
