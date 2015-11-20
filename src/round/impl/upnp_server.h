eb51d81dbb14c5c0405189c90c784df3200d4758
Satoshi Konno <skonno@cybergarage.org>
2015-11-20 10:03:37 +0900

    * Updated UPnP Server.

--- src/round/impl/upnp_server.h
+++ src/round/impl/upnp_server.h
@@ -31,6 +31,7 @@ typedef void(*ROUND_UPNP_SERVER_JSON_RPC_REQUEST_LISTENER)(mUpnpHttpRequest *htt
 typedef struct {
   mUpnpDevice *dev;
   ROUND_UPNP_SERVER_JSON_RPC_REQUEST_LISTENER rpcReqListener;
+  void *rpcServer;
 } RoundUpnpServer;
   
 /****************************************
@@ -41,15 +42,12 @@ RoundUpnpServer *round_upnp_server_new(void);
 bool round_upnp_server_delete(RoundUpnpServer *server);
 
 #define round_upnp_server_setrpcrequestlistener(server,func) (server->rpcReqListener = func)
-#define round_upnp_server_hasrpcrequestlistener(server) ((server->rpcReqListener) ? true : false)
+#define round_upnp_server_setrpcserver(server,func) (server->rpcReqListener = func)
 #define round_upnp_server_postrpcrequest(server,httpReq) server->rpcReqListener(httpReq)
 
 #define round_upnp_server_start(server) mupnp_device_start(server->dev)
 #define round_upnp_server_stop(server) mupnp_device_stop(server->dev)
 #define round_upnp_server_isrunning(server) mupnp_device_isrunning(server->dev)
-
-#define round_upnp_server_setrpcserver(server,rpcSrv) mupnp_device_setuserdata(server->dev, rpcSrv)
-#define round_upnp_device_getrpcserver(dev,rpcSrv) ((RoundRpcServer *)mupnp_device_getuserdata(dev))
   
 #ifdef  __cplusplus
 } /* extern C */
