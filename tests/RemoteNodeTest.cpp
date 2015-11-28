90923399942cf768323cea9c29905d7579ff6437
Satoshi Konno <skonno@cybergarage.org>
2015-11-28 19:06:08 +0900

    * Added round_node_equals().

--- tests/RemoteNodeTest.cpp
+++ tests/RemoteNodeTest.cpp
@@ -12,6 +12,8 @@
 
 #include <round/node_internal.h>
 
+BOOST_AUTO_TEST_SUITE(node)
+
 BOOST_AUTO_TEST_CASE(RemoteNodeNew)
 {
   RoundRemoteNode *node = round_remote_node_new();
@@ -19,3 +21,26 @@ BOOST_AUTO_TEST_CASE(RemoteNodeNew)
   
   BOOST_CHECK(round_remote_node_delete(node));
 }
+
+BOOST_AUTO_TEST_CASE(RemoteNodeCopy)
+{
+  const char *TEST_ADDR = "1.2.3.4";
+  const int TEST_PORT = 8080;
+  const char *TEST_CLUSTER = "round";
+  
+  RoundRemoteNode *node0 = round_remote_node_new();
+  BOOST_CHECK(node0);
+  round_remote_node_setaddress(node0, TEST_ADDR);
+  round_remote_node_setport(node0, TEST_PORT);
+  round_remote_node_setcluster(node0, TEST_CLUSTER);
+  
+  RoundRemoteNode *node1 = round_remote_node_copy((RoundNode *)node0);
+  BOOST_CHECK(node1);
+  
+  BOOST_CHECK(round_node_equals((RoundNode *)node0, (RoundNode *)node1));
+  
+  BOOST_CHECK(round_remote_node_delete(node0));
+  BOOST_CHECK(round_remote_node_delete(node1));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
