8bda2ef4a7146ea25ab913636d4fa26e1e2e8975
Satoshi Konno <skonno@cybergarage.org>
2015-12-10 02:19:41 +0900

    * Updated client functions.

--- tests/ClientTest.cpp
+++ tests/ClientTest.cpp
@@ -42,7 +42,6 @@ BOOST_AUTO_TEST_CASE(ClientStartAfterServer)
   Round::Test::Sleep();
   while (round_client_getclustersize(client) < 1) {
     BOOST_MESSAGE("Cluster is not found ...");
-    BOOST_CHECK(round_client_search(client));
     Round::Test::Sleep();
   }
   
@@ -55,5 +54,33 @@ BOOST_AUTO_TEST_CASE(ClientStartAfterServer)
 
   BOOST_CHECK(round_server_delete(server));
 }
+
+BOOST_AUTO_TEST_CASE(ClientStartBeforeServer)
+{
+  RoundClient* client = round_client_new();
+  BOOST_CHECK(client);
+  
+  RoundServer* server = round_server_new();
+  BOOST_CHECK(server);
+  
+  BOOST_CHECK(round_client_start(client));
+  BOOST_CHECK(round_server_start(server));
+  
+  Round::Test::Sleep();
+  while (round_client_getclustersize(client) < 1) {
+    BOOST_MESSAGE("Cluster is not found ...");
+    Round::Test::Sleep();
+  }
+  
+  BOOST_CHECK_EQUAL(round_client_getclustersize(client), 1);
+  
+  BOOST_CHECK(round_server_stop(server));
+  BOOST_CHECK(round_client_stop(client));
+  
+  BOOST_CHECK(round_client_delete(client));
+  
+  BOOST_CHECK(round_server_delete(server));
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
