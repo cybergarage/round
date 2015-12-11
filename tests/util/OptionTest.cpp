ee8334d98b29b010a79bf57d6c41ad2d831aba6c
Satoshi Konno <skonno@cybergarage.org>
2015-12-11 16:21:51 +0900

    * Added tests for option functions.

---
+++ tests/util/OptionTest.cpp
@@ -0,0 +1,43 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#include <boost/test/unit_test.hpp>
+
+#include <round/util/option.h>
+
+#define ROUND_TEST_LIST_SIZE 10
+
+BOOST_AUTO_TEST_SUITE(option)
+
+BOOST_AUTO_TEST_CASE(OptionSet)
+{
+  RoundOption opt;
+  
+  BOOST_CHECK_EQUAL(round_option_get(opt), 0);
+  BOOST_CHECK(round_option_isdisabled(opt, 0x01));
+  BOOST_CHECK(round_option_isdisabled(opt, 0x02));
+
+  round_option_set(opt, 0x01);
+  BOOST_CHECK_EQUAL(round_option_get(opt), 0x01);
+  BOOST_CHECK(round_option_isenabled(opt, 0x01));
+  BOOST_CHECK(round_option_isdisabled(opt, 0x02));
+
+  round_option_set(opt, 0x02);
+  BOOST_CHECK_EQUAL(round_option_get(opt), 0x02);
+  BOOST_CHECK(round_option_isdisabled(opt, 0x01));
+  BOOST_CHECK(round_option_isenabled(opt, 0x02));
+
+  round_option_set(opt, 0x03);
+  BOOST_CHECK_EQUAL(round_option_get(opt), 0x03);
+  BOOST_CHECK(round_option_isenabled(opt, 0x01));
+  BOOST_CHECK(round_option_isenabled(opt, 0x02));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
