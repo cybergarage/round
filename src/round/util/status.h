6d6ed442e9cfb55324463c023952f43167bd13d3
Satoshi Konno <skonno@cybergarage.org>
2015-12-11 16:34:35 +0900

    * Added status functions.

---
+++ src/round/util/status.h
@@ -0,0 +1,43 @@
+/******************************************************************
+ *
+ * Round for C
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _ROUND_UTIL_STATUS_H_
+#define _ROUND_UTIL_STATUS_H_
+
+#include <round/typedef.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+/****************************************
+ * Data Type
+ ****************************************/
+
+typedef int RoundStatus;
+  
+/****************************************
+ * Macros
+ ****************************************/
+
+#define round_status_set(stat, value) (stat = value)
+#define round_status_get(stat) (stat)
+#define round_status_setflag(stat, flag, value) (value ? (stat |= flag) : (stat ^= flag))
+#define round_status_isenabled(stat, flag) ((stat & flag) ? true : false)
+#define round_status_isdisabled(stat, flag) ((stat & flag) ? false : true)
+#define round_status_clear(stat) (stat = 0)
+
+#ifdef  __cplusplus
+
+} /* extern "C" */
+
+#endif
+
+#endif
