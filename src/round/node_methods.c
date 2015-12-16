a541e349a6ce832b8452cb25f7dc23095a02f94d
Satoshi Konno <skonno@cybergarage.org>
2015-12-16 23:18:20 +0900

    * Added round_node_removeregistry().

--- src/round/node_methods.c
+++ src/round/node_methods.c
@@ -111,3 +111,48 @@ bool round_node_getregistry(RoundNode* node, const char *key, char **value, Roun
   
   return isSuccess;
 }
+
+/****************************************
+ * round_node_removeregistry
+ ****************************************/
+
+bool round_node_removeregistry(RoundNode* node, const char *key, RoundError *err)
+{
+  if (!node || !key)
+    return false;
+  
+  // Create request
+  
+  RoundJSONObject *reqObj = round_json_rpc_request_new();
+  if (!reqObj) {
+    round_error_setjsonrpcerrorcode(err, ROUND_RPC_ERROR_CODE_INTERNAL_ERROR);
+    return false;
+  }
+  round_json_rpc_setmethod(reqObj, ROUND_SYSTEM_METHOD_REMOVE_REGISTRY);
+  
+  // Set params
+  
+  RoundJSONObject *paramsObj = round_json_map_new();
+  if (!paramsObj) {
+    round_error_setjsonrpcerrorcode(err, ROUND_RPC_ERROR_CODE_INTERNAL_ERROR);
+    round_json_object_delete(reqObj);
+    return false;
+  }
+  round_json_map_setstring(paramsObj, ROUND_SYSTEM_METHOD_PARAM_KEY, key);
+  round_json_rpc_setparams(reqObj, paramsObj);
+  
+  // Request
+  
+  RoundJSONObject *resObj = NULL;
+  bool isSuccess = round_node_postmessage(node, reqObj, &resObj, err);
+  
+  // Delete objects
+  
+  if (resObj) {
+    round_json_object_delete(resObj);
+  }
+  round_json_object_delete(reqObj);
+  
+  return isSuccess;
+}
+
