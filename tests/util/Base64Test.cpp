0c34172bb71024d376c91f595df6a09ffa0855be
Satoshi Konno <skonno@yahoo-corp.jp>
2016-03-09 18:13:50 +0900

    * Updated Base64 functions using OpenSSL.

--- tests/util/Base64Test.cpp
+++ tests/util/Base64Test.cpp
@@ -12,7 +12,9 @@
 
 #include <round/util/base64.h>
 
-/*
+ssize_t round_base64_encode(const byte *rawBytes, size_t rawByteLen, char **encodedStr);
+ssize_t round_base64_decode(const char *encodedStr, byte **decordedBytes);
+
 BOOST_AUTO_TEST_CASE(Base64Test)
 {
   std::vector<std::string> testStrings;
@@ -27,21 +29,20 @@ BOOST_AUTO_TEST_CASE(Base64Test)
   testStrings.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
   testStrings.push_back("+-=~`!@#$%^&*()_+{}[]|\\:;\"'<>,.?/");
 
-  for (std::vector<std::string>::iterator testString = testStrings.begin();
-testString != testStrings.end(); testString++) {
+  for (std::vector<std::string>::iterator testString = testStrings.begin(); testString != testStrings.end(); testString++) {
     byte *encordBytes = (byte *)(*testString).c_str();
     size_t ecordByteLen = (*testString).length();
-    std::string encodedStr;
-    BOOST_CHECK(0 <= Base64::Encode(encordBytes, ecordByteLen, &encodedStr));
+    char *encodedStr = NULL;
+    BOOST_CHECK(0 <= round_base64_encode(encordBytes, ecordByteLen, &encodedStr));
 
-    byte *decordedBytes;
-    size_t decordedByteLen = Base64::Decode(encodedStr, &decordedBytes);
+    byte *decordedBytes = NULL;
+    size_t decordedByteLen = round_base64_decode(encodedStr, &decordedBytes);
     BOOST_CHECK(0 <= decordedByteLen);
 
     std::string decordedString((const char *)decordedBytes, decordedByteLen);
     BOOST_CHECK_EQUAL((*testString).compare(decordedString), 0);
 
+    free(encodedStr);
     free(decordedBytes);
   }
 }
-*/
