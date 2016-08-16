798280055f020a36b8ec19541521895377a44ca8
Satoshi Konno <skonno@cybergarage.org>
2016-08-16 23:58:59 +0900

    * Updated trigger functions.

--- src/round/trigger.h
+++ src/round/trigger.h
@@ -25,9 +25,8 @@ extern "C" {
  * Data Type
  ****************************************/
 
-typedef struct {
-  RoundThread *thread;
-} RoundTrigger;
+typedef RoundThread RoundTrigger;
+typedef RoundThreadFunc RoundTriggerFunc;
 
 typedef struct {
   RoundThreadManager *threadMgr;
@@ -37,9 +36,24 @@ typedef struct {
  * Function
  ****************************************/
   
-RoundTrigger *round_trigger_new();
-bool round_trigger_clear(RoundTrigger *trigger);
-bool round_trigger_delete(RoundTrigger *trigger);
+#define round_trigger_new round_trigger_new
+#define round_trigger_delete(trigger) round_trigger_delete(trigger)
+
+#define round_trigger_setaction(trigger,func) round_thread_setaction(trigger,func)
+  
+#define round_trigger_setloop(thread,flag) round_thread_setloop(thread,flag)
+#define round_trigger_isloop(thread) round_thread_isloop(thread)
+  
+#define round_trigger_setcycleinterval(thread,value) round_thread_setcycleinterval(thread,value)
+#define round_trigger_getcycleinterval(thread) round_thread_getcycleinterval(thread)
+
+#define round_trigger_setstarttime(thread,value) round_thread_setstarttime(thread,value)
+#define round_trigger_getstarttime(thread) round_thread_getstarttime(thread)
+#define round_trigger_isstarttimeenabled(thread) round_thread_isstarttimeenabled(thread)
+  
+#define round_trigger_setstoptime(thread,value) round_thread_setstoptime(thread,value)
+#define round_trigger_getstoptime(thread) round_thread_getstoptime(thread)
+#define round_trigger_isstoptimeenabled(thread) round_thread_isstoptimeenabled(thread)
 
 /****************************************
  * Function (Manager)
--- tests/TriggerTest.cpp
+++ tests/TriggerTest.cpp
@@ -8,4 +8,17 @@
  *
  ******************************************************************/
 
+#include <boost/test/unit_test.hpp>
+
 #include <round/trigger.h>
+
+BOOST_AUTO_TEST_SUITE(trigger)
+
+BOOST_AUTO_TEST_CASE(TriggerNew)
+{
+  RoundTrigger *trigger = round_trigger_new();
+  
+  round_trigger_delete(trigger);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
