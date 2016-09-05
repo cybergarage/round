8b34260e70176a8b1fb97d347ad098e5d4f28703
Satoshi Konno <skonno@cybergarage.org>
2016-09-05 14:52:52 +0900

    * Added a unittest for object.

--- tests/util/ObjectTest.cpp
+++ tests/util/ObjectTest.cpp
@@ -99,5 +99,51 @@ BOOST_AUTO_TEST_CASE(ObjecReleaseReferenceByThread)
   BOOST_CHECK_EQUAL(round_thread_delete(thread), true);
 }
 
+static void RoundTestObjectRandomAllocReference(RoundThread *thread)
+{
+  RoundObject *obj = (RoundObject *)round_thread_getuserdata(thread);
+  for (int n=0; n < ROUND_TEST_LOOP_COUNT; n++) {
+    round_waitrandom(0.1);
+    round_object_alloc(obj);
+  }
+}
+
+static void RoundTestObjectRandomReleaseReference(RoundThread *thread)
+{
+  RoundObject *obj = (RoundObject *)round_thread_getuserdata(thread);
+  for (int n=0; n < ROUND_TEST_LOOP_COUNT; n++) {
+    round_waitrandom(0.1);
+    round_object_release(obj);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(ObjecRandomAllocReleaseReferenceByThread)
+{
+  RoundObject* obj = round_object_new();
+  
+  BOOST_CHECK(obj);
+  
+  BOOST_CHECK(round_object_alloc(obj));
+  
+  RoundThread *thread0 = round_thread_new();
+  round_thread_setaction(thread0, RoundTestObjectRandomAllocReference);
+  round_thread_setuserdata(thread0, obj);
+  BOOST_CHECK_EQUAL (round_thread_start(thread0), true);
+  
+  RoundThread *thread1 = round_thread_new();
+  round_thread_setaction(thread1, RoundTestObjectRandomReleaseReference);
+  round_thread_setuserdata(thread1, obj);
+  BOOST_CHECK_EQUAL (round_thread_start(thread1), true);
+  
+  BOOST_CHECK(round_object_release(obj));
+  
+  BOOST_CHECK(round_object_delete(obj));
+  
+  BOOST_CHECK_EQUAL(round_thread_stop(thread0), true);
+  BOOST_CHECK_EQUAL(round_thread_stop(thread1), true);
+  
+  BOOST_CHECK_EQUAL(round_thread_delete(thread0), true);
+  BOOST_CHECK_EQUAL(round_thread_delete(thread1), true);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
