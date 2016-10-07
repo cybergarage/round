1cc4dcc49b48c7286fd42c7027a932f76fdce353
Satoshi Konno <skonno@cybergarage.org>
2016-10-07 21:28:04 +0900

    * Added ROUND_TEST_RETRY_* macros.

--- tests/RoundTest.h
+++ tests/RoundTest.h
@@ -16,6 +16,10 @@
 #define ROUND_TEST_DEFAULT_WAIT_MILLI_SECOND 500
 #define ROUND_TEST_ADDR "1.2.3.4"
 #define ROUND_TEST_PORT 8080
+#define ROUND_TEST_RETRY_COUNT 10
+
+#define ROUND_TEST_RETRY_COUNT_INIT(var) size_t var = 0;
+#define ROUND_TEST_RETRY_COUNT_CHECK(var) if (ROUND_TEST_RETRY_COUNT < ++var) {break;}
 
 namespace Round {
   
