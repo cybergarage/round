534c95f8bc36cad0418f5a66644aa2344d56d278
Satoshi Konno <skonno@cybergarage.org>
2016-10-20 19:39:50 +0900

    * Fixed typos.

--- src/round/trigger.h
+++ src/round/trigger.h
@@ -45,19 +45,19 @@ RoundTrigger *round_trigger_new(void);
 
 #define round_trigger_setaction(trigger,func) round_thread_setaction(trigger,func)
   
-#define round_trigger_setloop(thread,flag) round_thread_setloop(thread,flag)
-#define round_trigger_isloop(thread) round_thread_isloop(thread)
+#define round_trigger_setloop(trigger,flag) round_thread_setloop(trigger,flag)
+#define round_trigger_isloop(trigger) round_thread_isloop(trigger)
   
-#define round_trigger_setcycleinterval(thread,value) round_thread_setcycleinterval(thread,value)
-#define round_trigger_getcycleinterval(thread) round_thread_getcycleinterval(thread)
+#define round_trigger_setcycleinterval(trigger,value) round_thread_setcycleinterval(trigger,value)
+#define round_trigger_getcycleinterval(trigger) round_thread_getcycleinterval(trigger)
 
-#define round_trigger_setstarttime(thread,value) round_thread_setstarttime(thread,value)
-#define round_trigger_getstarttime(thread) round_thread_getstarttime(thread)
-#define round_trigger_isstarttimeenabled(thread) round_thread_isstarttimeenabled(thread)
+#define round_trigger_setstarttime(trigger,value) round_thread_setstarttime(trigger,value)
+#define round_trigger_getstarttime(trigger) round_thread_getstarttime(trigger)
+#define round_trigger_isstarttimeenabled(trigger) round_thread_isstarttimeenabled(trigger)
   
-#define round_trigger_setstoptime(thread,value) round_thread_setstoptime(thread,value)
-#define round_trigger_getstoptime(thread) round_thread_getstoptime(thread)
-#define round_trigger_isstoptimeenabled(thread) round_thread_isstoptimeenabled(thread)
+#define round_trigger_setstoptime(trigger,value) round_thread_setstoptime(trigger,value)
+#define round_trigger_getstoptime(trigger) round_thread_getstoptime(trigger)
+#define round_trigger_isstoptimeenabled(trigger) round_thread_isstoptimeenabled(trigger)
 
 #define round_trigger_setroutemanager(mgr,data) round_thread_setuserdata(mgr,data)
 #define round_trigger_getroutemanager(mgr) ((RoundRouteManager *)round_thread_getuserdata(mgr))
